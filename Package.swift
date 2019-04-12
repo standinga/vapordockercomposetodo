653121a20ab9e2e112746f67b9295ad3e1971d36
michal bojanowicz <boramaapps@gmail.com>
2019-04-12 19:33:44 +0200

    working version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
@@ -7,14 +7,11 @@ let package = Package(
         .library(name: "server", targets: ["App"]),
     ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentMySQL", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,32 +1,52 @@
-import FluentSQLite
+import FluentMySQL
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
-    try services.register(FluentSQLiteProvider())
-
+    try services.register(FluentMySQLProvider())
+    
     // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
-
+    
     // Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
-
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
-
-    // Register the configured SQLite database to the database config.
+    
+    let config = databaseConfig()
+    let database = MySQLDatabase(config: config)
+    
+    // Register the configured MySQL database to the database config.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    databases.add(database: database, as: .mysql)
     services.register(databases)
-
+    
     // Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
+    migrations.add(model: Todo.self, database: .mysql)
     services.register(migrations)
+}
+
+func databaseConfig() -> MySQLDatabaseConfig {
+    // read environemnt vars and print them for debugging:
+    let envHost = Environment.get("DATABASE_HOST")
+    let envPort = Environment.get("MYSQL_PORT")
+    let envUserName = Environment.get("MYSQL_USER")
+    let envPass = Environment.get("MYSQL_PASSWORD")
+    let evnDBName = Environment.get("MYSQL_DATABASE")
+    
+    print("envHost: \(String(describing: envHost)) envPort: \(String(describing: envPort)) envUserName: \(String(describing: envUserName)) envPass: \(String(describing: envPass)) evnDBName: \(String(describing: evnDBName))")
+    
+    let hostName = envHost ?? "0.0.0.0"
+    let databasePort = Int(envPort ?? "3306") ?? 3306
+    let userName = envUserName ?? "test"
+    let password = envPass ?? "test"
+    let databaseName = evnDBName ?? "apps"
+    
+    let config = MySQLDatabaseConfig(hostname: hostName, port: databasePort, username: userName, password: password, database: databaseName)
+    return config
 }
